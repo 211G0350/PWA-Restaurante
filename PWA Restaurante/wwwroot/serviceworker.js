5872c5f8251d46f90e2cc3a0701eec0bda0af6db
Esquivel Rodriguez Leonardo Valentin <211G0350@rcarbonifera.tecnm.mx>
2025-11-20 12:25:37 -0600

    quitar vistas no necesarias, e intentaré hacer el serviceworker y manifest ya tras esto

---
+++ PWA Restaurante/wwwroot/serviceworker.js
@@ -0,0 +1,36 @@
+﻿/*
+
+let urls = [
+    "Home/index.cshtml",
+ //   "api/Admin",
+    "/"
+];
+
+
+async function descargarInstalar() {
+    let cache = await caches.open("cachePlanes");
+    //cache.put("api/carreras");
+    await cache.addAll(urls);
+    self.skipWaiting();
+}
+
+async function obtenerDesdeCache(request) {
+    let cache = await caches.open("cachePlanes");
+    let response = await cache.match(request);
+    if (response) {
+        return response;
+    }
+    else {
+        return await fetch(request);
+    }
+}
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(descargarInstalar());
+});
+
+//Funcion fetch (recuperar datos)
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(obtenerDesdeCache(event.request));
+});*/
